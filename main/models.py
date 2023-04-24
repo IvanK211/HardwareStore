781f4b2a90c51bac943a17029fccdf56d194dd93
IvanK211 <b.kochnev@acg.edu>
2023-04-24 19:57:31 +0300

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.db.models import Sum
+
+class Product(models.Model):
+	product_name = models.CharField(max_length=150)
+	product_type = models.CharField(max_length=25)
+	product_description = models.TextField()
+	affiliate_url = models.SlugField(blank=True, null=True)
+	product_image = models.ImageField(upload_to='images/')
+	pricing_average = models.DecimalField(default=0, max_digits=3, decimal_places=1)
+	performance_average = models.DecimalField(default=0, max_digits=3, decimal_places=1)
+	durability_average = models.DecimalField(default=0, max_digits=3, decimal_places=1)
+
+
+	def __str__(self):
+		return self.product_name
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	products = models.ManyToManyField(Product)
+
+	@receiver(post_save, sender=User) #add this
+	def create_user_profile(sender, instance, created, **kwargs):
+		if created:
+			Profile.objects.create(user=instance)
+
+	@receiver(post_save, sender=User) #add this
+	def save_user_profile(sender, instance, **kwargs):
+		instance.profile.save()
+
+class Vote(models.Model):   #add this class and the following fields
+	profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	pricing = models.IntegerField(default=0)
+	performance = models.IntegerField(default=0)
+	durability = models.IntegerField(default=0)
+
+	def calculate_averages(self):
+			product = self.product
+			vote_qs = Vote.objects.filter(product=product)
+			vote_count = vote_qs.count()
+			pricing_total = vote_qs.aggregate(Sum('pricing'))
+			performance_total = vote_qs.aggregate(Sum('performance'))
+			durability_total = vote_qs.aggregate(Sum('durability'))
+			product.pricing_average = pricing_total['pricing__sum']/vote_count
+			product.performance_average = performance_total['performance__sum']/vote_count
+			product.durability_average = durability_total['durability__sum']/vote_count
+			product.save()
+
